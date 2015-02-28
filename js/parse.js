3c7651333631ba6f0d11f9a914e333736f5edb53
LiamRahav <liorogamerdvd@gmail.com>
2015-02-28 18:35:08 -0500

    removed stuff

--- js/parse.js
+++
@@ -1,7 +0,0 @@
-Parse.initialize("TckU2eAUVMu27DCKVA2YSL1aPdl3vEffBO7pl2D7", "iPRlEEyjGAGl4WKDZb0EU28Tn3zoqVIp9chNew4b");
-
-var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}).then(function(object) {
-  alert("yay! it worked");
-});

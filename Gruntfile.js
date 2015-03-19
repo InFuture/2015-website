d59c0ba1c757d6b0cd0e1f80fabe83296e87c399
Paul Vorobyev <paul.vorobyev@gmail.com>
2015-03-18 21:14:45 -0400

    begin refactor

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        sass: {
+            compile: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: 'none'
+                },
+                files: {
+                    'css/styles.css': 'css/importer.scss'
+                }
+            }
+        },
+        watch: {
+            style: {
+                files: 'css/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass'); 
+
+    grunt.registerTask('default', ['sass:compile','watch']);
+}

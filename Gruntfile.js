09b808c369a88653d79b4eb52f24c5f66cd00862
BrandonDGr8 <brandonthefirst597@gmail.com>
2015-03-19 23:55:57 -0400

    open sans via google fonts & grunt fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt){
         },
         watch: {
             style: {
-                files: 'css/*.scss',
+                files: "css/**/*.scss",
                 tasks: ['sass']
             }
         }

ed545e8345578b89b4aeb2e6d95efb06ac9cbaa6
BrandonDGr8 <brandonthefirst597@gmail.com>
2015-03-23 02:54:26 -0400

    fully responsive menu - hamburger a little broken on mobile

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-  $("#menuButton").click(function(){
+  $("#menuButton").click(function(event){
     $("#menuButton").hide();
     $("#closeButton").show("fast");
     $("#navS").fadeTo("fast", 1);
+    event.stopPropagation();
   });
   $("#closeButton").click(function(){
     $(this).hide("fast");
@@ -11,6 +12,15 @@ $(document).ready(function(){
     $("#navS").hide("fast");
   });
   $(".button").click(function(){
+    $("#closeButton").hide("fast");
+    $("#menuButton").show();
+    $("#navS").fadeTo("fast", 0);
+    $("#navS").hide("fast");
+  });
+  $("#navS").click(function(event){
+    event.stopPropagation();
+  });
+  $("html").click(function(){
     $("#closeButton").hide("fast");
     $("#menuButton").show();
     $("#navS").fadeTo("fast", 0);

2e13400bd416ee43191f6f054ef0333ec98a267f
BrandonDGr8 <brandonthefirst597@gmail.com>
2015-03-23 01:04:01 -0400

    margin changes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-//scroll effect to anchors
-$(function() {
- $('a[href*=#]:not([href=#])').click(function() {
-   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-
-     var target = $(this.hash);
-     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-     if (target.length) {
-       $('html,body').animate({
-         scrollTop: target.offset().top
-       }, 1000);
-       return false;
-     }
-   }
- });
+$(document).ready(function(){
+  $("#menuButton").click(function(){
+    $("#menuButton").hide();
+    $("#closeButton").show("fast");
+    $("#navS").fadeTo("fast", 1);
+  });
+  $("#closeButton").click(function(){
+    $(this).hide("fast");
+    $("#menuButton").show();
+    $("#navS").fadeTo("fast", 0);
+    $("#navS").hide("fast");
+  });
+  $(".button").click(function(){
+    $("#closeButton").hide("fast");
+    $("#menuButton").show();
+    $("#navS").fadeTo("fast", 0);
+    $("#navS").hide("fast");
+  });
 });

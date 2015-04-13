3d9608c0b885dda5f50509444d7aeb6872f475c6
Paul Vorobyev <paul.vorobyev@gmail.com>
2015-04-13 01:36:02 -0400

    change directory structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,20 +7,44 @@ module.exports = function(grunt){
                     sourcemap: 'none'
                 },
                 files: {
-                    'css/styles.css': 'css/importer.scss'
+                    'dist/css/styles.css': 'src/css/importer.scss'
                 }
             }
         },
+
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'src/index.html'
+                }
+            }
+        },
+
+        uglify: {
+            minify: {
+                files: {
+                    'dist/js/script.js': ['src/js/bootstrap.min.js', 'src/js/script.js']
+                }
+            }
+        },
+
         watch: {
             style: {
                 files: "css/**/*.scss",
-                tasks: ['sass']
+                tasks: ['sass', 'htmlmin']
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass'); 
+    grunt.loadNpmTasks('grunt-contrib-htmlmin'); 
+    grunt.loadNpmTasks('grunt-contrib-concat'); 
+    grunt.loadNpmTasks('grunt-contrib-uglify'); 
 
-    grunt.registerTask('default', ['sass:compile','watch']);
+    grunt.registerTask('default', ['sass:compile', 'uglify', 'htmlmin', 'watch']);
 }
